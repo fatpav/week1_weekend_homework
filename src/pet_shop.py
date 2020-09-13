a73b9626c7b49a8bc894315715642d7bfcbc8350
fatpav <fat_pav@hotmail.com>
2020-09-13 22:08:23 +0100

    first commit, I'm really struggling here

---
+++ src/pet_shop.py
@@ -0,0 +1,24 @@
+# WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"] 
+
+def add_or_remove_cash(pet_shop, cash_transaction):
+    pet_shop["admin"]["total_cash"] = pet_shop["admin"]["total_cash"] + cash_transaction
+    return pet_shop["admin"]["total_cash"]
+
+    
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, new_sales):
+    pet_shop["admin"]["pets_sold"] = pet_shop["admin"]["pets_sold"] + new_sales
+    return pet_shop["admin"]["pets_sold"]
+
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop, breed_type):
+    
