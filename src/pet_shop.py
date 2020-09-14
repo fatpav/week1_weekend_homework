279c8d31511d1a30d403d025a2716c2b21f6c368
fatpav <fat_pav@hotmail.com>
2020-09-14 11:14:08 +0100

    third commit, getting there but still no real idea

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -6,21 +6,26 @@ def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"] 
 
 def add_or_remove_cash(pet_shop, cash_transaction):
-    pet_shop["admin"]["total_cash"] = pet_shop["admin"]["total_cash"] + cash_transaction
-    return pet_shop["admin"]["total_cash"]
-
+    pet_shop["admin"]["total_cash"] += cash_transaction
+    
     
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
 
 def increase_pets_sold(pet_shop, new_sales):
-    pet_shop["admin"]["pets_sold"] = pet_shop["admin"]["pets_sold"] + new_sales
-    return pet_shop["admin"]["pets_sold"]
+    pet_shop["admin"]["pets_sold"] += new_sales
 
 def get_stock_count(pet_shop):
-    return len(pet_shop["pets"])
+    return len(pet_shop["pets"])   
 
 def get_pets_by_breed(pet_shop, breed_type):
-    from collections import Counter
-    breed_number = Counter(pet_shop["pets"]["breed"] == breed_type)
-    return breed_number
+    # function needs to return a list
+    # I need to grab breed names from pet shop and find the matching breed type
+    # they need to go into the list and return the list
+    found_pets = []
+    pets = pet_shop["pets"]
+    for pet in pets:
+        if pet["breed"] == breed_type:
+            found_pets.append(pet)
+    return found_pets
+# this is going to add the matching pet breeds to the new list
