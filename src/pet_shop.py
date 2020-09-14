e389d8663b52122edb0072e5fa873ffda9e138b2
fatpav <fat_pav@hotmail.com>
2020-09-14 20:43:58 +0100

    all tests passed by mine own hand

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -47,3 +47,11 @@ def add_pet_to_stock(pet_shop, added_pet):
 def get_customer_cash(customer):
     return customer["cash"]
 
+def remove_customer_cash(customer, transaction_amount):
+    customer["cash"] -= transaction_amount
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
